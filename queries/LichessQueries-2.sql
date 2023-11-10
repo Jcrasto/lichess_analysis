14c9fa976629dc712ec832c6dd6370af41ee7f9d
joshcrasto <jcrasto@g.ucla.edu>
2023-11-09 17:34:48 -0800

    adding new code for pgn_parser and new table sql"

--- queries/LichessQueries-2.sql
+++ queries/LichessQueries-2.sql
@@ -12,6 +12,7 @@ from lichess.lichess_api_data
 order by date desc, utctime desc 
 
 
+create view lichess.luckleland_results  as( 
 select "date", white, black, "result", 1 as count,
 case
 	when white = 'luckleland' and result = '1-0' then 1
@@ -26,8 +27,49 @@ end as "loss",
 case
 	when result = '1/2-1/2' then 1
 	else 0
-end as "draw",
-gamestring, id_key
+end as "draw", id_key
 from lichess.lichess_api_data
+)
+
+
+select *  
+from lichess.luckleland_results 
+where date >= '2023-06-01'
 order by date desc
 
+select date, sum(count) "count", sum(win)"win", sum(loss)"loss", sum(draw) "draw"
+from lichess.luckleland_results 
+where date >= '2023-06-01'
+group by date
+order by date desc
+
+
+SELECT
+    a.pgn_string,
+    SUM(b.count) AS total_games,
+    cast(SUM(b.win) as double)/ cast(SUM(b.count)as double) AS win,
+    cast(SUM(b.loss) as double) / cast(SUM(b.count) as double) AS loss,
+    cast(SUM(b.draw) as double) / cast(SUM(b.count) as double) AS draw
+FROM
+    lichess.running_gamestrings a
+LEFT JOIN
+    lichess.luckleland_results b
+ON
+    a.id_key = b.id_key
+WHERE
+    a.date >= '2023-01-01'
+    AND a.move_number = '1b'
+GROUP BY
+    a.pgn_string
+ORDER BY
+    loss DESC;
+   
+ 
+ select count()
+   
+ select move_number, count(distinct pgn_string)
+ from lichess.running_gamestrings 
+ group by move_number 
+ order by move_number 
+ 
+ 
